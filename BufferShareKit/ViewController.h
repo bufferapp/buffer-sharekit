d60622c891662c062edbe35a84e5e4b1d50c95b0
Andrew Yates <contact@andydev.co.uk>
2012-03-24 11:33:54 +0000

    Buffer for Sharekit Init

---
+++ BufferShareKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BufferShareKit
+//
+//  Created by Andrew Yates on 23/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BufferShareKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BufferShareKit
+//
+//  Created by Andrew Yates on 23/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
